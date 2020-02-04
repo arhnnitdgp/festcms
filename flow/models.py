5ba268da7eb2e179296e6268f99cb4e9a3885d34
revanth-reddy <malerevanthreddy3099@gmail.com>
2020-02-04 19:48:16 +0530

    files

--- flow/models.py
+++ flow/models.py
@@ -21,7 +21,7 @@ class Events(models.Model):
     contact = models.CharField(max_length=50)
     registration_open = models.BooleanField(default=True)
     registration_link = models.CharField(max_length=50,blank=True,help_text="Enter the registration link here")
-
+    doc = models.FileField(upload_to='pdfs/', blank=True)
     def __str__(self):
         return self.eventName
 
